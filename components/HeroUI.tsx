65c49ea4aa37341087c656888aa9952c8da4af0b
Muhammad Ayaan <kuchicheena@gmail.com>
2026-02-21 17:29:42 +0500

    fix: Update image source path in HeroUI component for consistency

--- components/HeroUI.tsx
+++ components/HeroUI.tsx
@@ -31,7 +31,7 @@ const HeroUI: React.FC = () => {
           onMouseLeave={() => setCursorVariant('default')}
         >
           <img 
-            src="/profile.jpg" 
+            src="profile.jpg" 
             alt="Muhammad Ayaan" 
             className="w-full h-full object-cover rounded-2xl opacity-80 grayscale group-hover:grayscale-0 group-hover:opacity-100 transition-all duration-700"
           />
